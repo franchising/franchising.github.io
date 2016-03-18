8ebcd901c8638dc41dfa9e7273b6680b547a56c7
franchising <sashuk_sawa@mail.ru>
2016-03-18 16:39:31 +0200

    second

---
+++ js/common.js
@@ -0,0 +1,114 @@
+$(function() {
+
+
+
+$('.main_mnu ul a').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top
+      }, 1000);
+
+      //
+      var wid = $(window).width();
+      if (wid <= 991) {
+      	  $(".main_mnu").fadeOut(600);
+      $(".sandwich").toggleClass("active");
+      }
+    
+      //
+      e.preventDefault();
+   });
+
+
+
+
+$('.popup-modal').magnificPopup();
+
+	$('img.img-svg').each(function(){
+    var $img = $(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
+
+
+	$(".toggle_mnu").click(function() {
+  		$(".sandwich").toggleClass("active");
+  		if($(".main_mnu").is(":hidden")){
+  				$(".main_mnu").fadeIn(600);
+  		}else{
+  			$(".main_mnu").fadeOut(600);
+  		}
+	});
+	$(window).resize(function(){
+		var wid = $(window).width();
+		if(wid > 991 && $(".main_mnu").is(':hidden')){
+			$(".main_mnu").removeAttr('style');
+		}
+	});
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+$(".preloader").fadeOut();
+});
